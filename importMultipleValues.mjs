b8aa47a5ef382bbdad8a97a38e300608394cff66
ajaydevra <ajaydevra1999@gmail.com>
2023-07-16 04:17:21 +0000

    solution

---
+++ importMultipleValues.mjs
@@ -0,0 +1,8 @@
+import {myArray,bulb,multiplyByFour} from "./exportMultipleValues.mjs";
+
+console.log(myArray);
+console.log(bulb);
+console.log(multiplyByFour(10));
+
+// import multiplyByFour from "./exportMultipleValues.mjs";
+// console.log(multiplyByFour(4));
